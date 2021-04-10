bc5b93f91033b863d95b9fd0fe0defdea9348650
nixcode1 <amaechi.chukwuebuka1@gmail.com>
2021-04-10 18:55:55 +0100

    added responsive framework to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:examiner/data/bindings/home_binding.dart';
 import 'package:examiner/ui/screens/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import 'data/navigation/app_pages.dart';
 
@@ -32,6 +33,19 @@ class MyApp extends StatelessWidget {
         // Once complete, show your application
         if (snapshot.connectionState == ConnectionState.done) {
           return GetMaterialApp(
+            builder: (context, widget) => ResponsiveWrapper.builder(
+                BouncingScrollWrapper.builder(context, widget!),
+                maxWidth: 1200,
+                minWidth: 450,
+                defaultScale: true,
+                breakpoints: [
+                  ResponsiveBreakpoint.resize(450, name: MOBILE),
+                  ResponsiveBreakpoint.autoScale(800, name: TABLET),
+                  ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+                  ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+                  ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+                ],
+                background: Container(color: Color(0xFFF5F5F5))),
             title: 'Examiner',
             debugShowCheckedModeBanner: false,
             initialRoute: Routes.INITIAL,
